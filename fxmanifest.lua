361acc12bedfd93d48f78f404e2ee9f0db882d23
szaraltforallblackpeople <adamgejus1337@gmail.com>
2025-06-30 11:50:58 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,8 @@ game 'gta5'
 
 author 'kriss'
 
+version '2.0' -- updated 30.06.2025 ( created somethin like 1-2 years ago, soon maybe new update to fully fix this shit ) 
+
 lua54 'yes'
 
 client_script {
